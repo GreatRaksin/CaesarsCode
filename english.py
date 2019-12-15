1eea79ddc311054d416a8f272349fa65a26077e8
Architect <greatraksin@icloud.com>
2019-12-15 10:08:35 +0300

    adding english script

--- english.py
+++ english.py
@@ -1,32 +1,33 @@
-symbols = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюя'
-variants = ['зашифровать', 'з', 'расшифровать', 'р', 'hfcibahjdfnm', 'h', 'pfibahjdfnm', 'p']
+symbols = 'abcdefghijklmnopqrstuvwxyz'
+variants = ['encrypt', 'decrypt', 'e', 'd']
 maxKeySize = len(symbols)
 
 
 def getMode():
     while True:
-        mode = input('Вы хотите зашифровать или расшифровать? \n').lower()
+        mode = input('Do you want to encrypt or decrypt message? \n').lower()
         if mode in variants:
             return mode
         else:
-            print(f'Введите "{variants[0]}" или "{variants[1]}" для зашифровки. \nВведите "{variants[2]}" или "{variants[3]}" для расшифровки.')
+            print(
+                f'input "{variants[0]}" or "{variants[2]}" for encryption. \nInput "{variants[1]}" or "{variants[3]}" for decription.')
 
 
 def getMessage():
-    msg = input('Введите текст: ')
+    msg = input('Input message: ')
     return msg
 
 
 def getKey():
     key = 0
     while True:
-        key = int(input(f'Введите ключ шифрования (1-{maxKeySize})'))
+        key = int(input(f'Input encryption key (1-{maxKeySize})'))
         if 1 <= key <= maxKeySize:
             return key
 
 
 def getTranslatedMessage(mode, msg, key):
-    if mode[0] == 'р':
+    if mode[0] == 'd':
         key = -key
     translated = ''
 
@@ -35,7 +36,7 @@ def getTranslatedMessage(mode, msg, key):
         if symbolIndex == -1:  # символ не найден
             # просто добавить этот символ без изменений
             translated += symbol
-        else: # Зашифровать или расшифровать
+        else:  # Зашифровать или расшифровать
             symbolIndex += key
 
             if symbolIndex >= len(symbols):
@@ -50,5 +51,4 @@ def getTranslatedMessage(mode, msg, key):
 mode = getMode()
 message = getMessage()
 key = getKey()
-print(f'Преобразованный текст: {getTranslatedMessage(mode, message, key)}')
-
+print(f'Your text: {getTranslatedMessage(mode, message, key)}')
